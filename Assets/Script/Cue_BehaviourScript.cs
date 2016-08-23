7afd13f8650af0b254b15637bbcc4253e2752423
poppen75 <poppen.sky3232@gmail.com>
2016-08-24 00:55:48 +0900

    cue&ball衝突実験
    (´ﾟｪﾟ`)オォン

---
+++ Assets/Script/Cue_BehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cue_BehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 v = this.transform.position;
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			v.x -= 0.05f;
+		}
+		if (Input.GetKey(KeyCode.RightArrow)){
+			v.x += 0.05f;
+		}
+		if (Input.GetKey(KeyCode.UpArrow)){
+			v.z += 0.05f;
+		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			v.z -= 0.05f;
+		}
+		this.transform.position = v;
+	}
+}
